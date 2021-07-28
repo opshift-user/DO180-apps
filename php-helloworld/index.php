9e69271428a676252f07039179d2d9d879a26713
Student User <student@workstation.lab.example.com>
2021-07-28 14:44:28 -0400

    Lampros is changing the n with br

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is in the air!<br />";
-print "Lampros was here :P<br /><br /><br />";	
+print "Lampros was here :P<br /><br /><br />";
+print "De kserw giati de thelei ta br, an to valw etsi? <br> ? apo tin alli exw kai kairo na asxolithw me php";
 ?>
